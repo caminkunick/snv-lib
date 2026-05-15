cd9d88c0b5d64601b4eb6097003512a3961b49af
CamINKu <caminkunick@gmail.com>
2026-05-15 16:58:57 +0700

    Add migration 20260515_095803 with up and down functions
    - Created a new migration file for 20260515_095803.
    - Implemented the up and down functions for the migration.
    - Updated the migrations index to include the new migration.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260514_070458 from './20260514_070458';
 import * as migration_20260515_094929 from './20260515_094929';
+import * as migration_20260515_095803 from './20260515_095803';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260515_094929.up,
     down: migration_20260515_094929.down,
-    name: '20260515_094929'
+    name: '20260515_094929',
+  },
+  {
+    up: migration_20260515_095803.up,
+    down: migration_20260515_095803.down,
+    name: '20260515_095803'
   },
 ];
